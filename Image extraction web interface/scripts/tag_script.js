85855792014ea60ac148676a85db6d1da256cf41
carlAbennett <bennetca@sunyit.edu>
2018-09-13 13:10:51 -0400

    Add files via upload

---
+++ Image extraction web interface/scripts/tag_script.js
@@ -0,0 +1,13 @@
+
+function set_warning(){
+	document.getElementById("warning").onmouseover= function(){document.getElementById("warning").style.display="none";};
+}
+
+function auto_submit(){
+	document.getElementById("auto").submit();
+}
+
+
+function check(id){
+	document.getElementById(id).checked=true;
+}
